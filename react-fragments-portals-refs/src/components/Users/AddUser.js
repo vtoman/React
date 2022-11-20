d246dff46103c01635a5fd29e258cf1d209d9521
vtoman <vtoman@gmail.com>
2022-11-20 16:21:15 +0100

    Adding fragment instead of div to cleanup dom.

--- react-fragments-portals-refs/src/components/Users/AddUser.js
+++ react-fragments-portals-refs/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -44,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Fragment>
   );
 };
 
